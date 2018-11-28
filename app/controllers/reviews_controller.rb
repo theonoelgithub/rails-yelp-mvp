bf2c456e333ae197427c4a71b3c269fb9c81def9
Theo Noel <theo.noel59@gmail.com>
2018-11-28 15:13:06 +0100

    fixing issue with the error messages @validation of reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,10 +11,9 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render 'restaurants/show'
     end
   end
-
 
   private
   # Private Methods below
